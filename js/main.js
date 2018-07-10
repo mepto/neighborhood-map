adc204e42554ffd544344e71be6116a360cb8af5
Nathalie <natg.inf@gmail.com>
2018-07-10 07:40:17 +0200

    Add ko bindings on sidenav, start working on locations list

--- js/main.js
+++ js/main.js
@@ -1,360 +1,63 @@
-//(function () {
-// sidebar toggle
-//var sidebarButton = document.getElementById('sidebarToggle');
-//sidebarButton.addEventListener('click', toggleSidebar, false);
-//
-//function toggleSidebar() {
-//    var sidebar = document.getElementById('sidebar');
-//    var sidebarButtonText = document.getElementById('sidebarButtonText');
-//    sidebar.classList.toggle('active');
-//    sidebarButton.classList.toggle('open');
-//    if (sidebarButton.classList.contains('open')) {
-//        sidebarButtonText.innerHTML = 'Hide list';
-//    } else {
-//        sidebarButtonText.innerHTML = 'Show list';
-//    }
-//}
-// -- end sidebar toggle
+ //(function () {
 
 
 function AppVMButton () {
     var btnOpen = 'Show places';
     var btnClosed = 'Hide Places';
+    var sidebarClass = 'active';
     btnText = ko.observable(btnOpen);
-    toggleClasses = function() {
-        if (btnText() == btnOpen) {
-            btnText(btnClosed);
-        } else {
-            btnText(btnOpen);
-        }
-//        btnText(btnText(btnClosed) ? btnOpen : btnClosed);
+    classActive = ko.observable("");
+    toggleButtonText = function() {
+        btnText() == btnClosed ? btnText(btnOpen) : btnText(btnClosed)
     };
+    toggleButtonClass = ko.computed(function(){
+        return btnText() == btnClosed ? "open" : " ";
+    });
+    toggleMenuClass = ko.computed(function() {
+        return btnText() == btnClosed ? "active" : " ";
+    });
+}
+
+//var Location = function(location){
+//    this.title = location.title;
+//    this.lat = location.lat;
+//    this.lng = location.lng;
+//}
+
+//https://www.strathweb.com/2012/07/knockout-js-pro-tips-working-with-observable-arrays/
+//https://stackoverflow.com/questions/47741328/filtering-list-with-knockout
+function AppVMLocations () {
+    var self = this;
+    self.filterText = ko.observable('');
+    self.allLocations = ko.observableArray(locations);
+    self.filteredLocations = ko.computed(function(){
+        if (self.filterText().length > 0) {
+            var locationsArray = self.allLocations();
+            return ko.utils.arrayFilter(teamsArray, function(allLocations){
+                return ko.utils.stringStartsWith(allLocations.locations.toLowerCase(), self.filterText());
+            });
+        }
+    }).extend({ throttle: 750 });
+
+//    locations.forEach(function(loc){
+//        self.allLocations.push(new Location(loc));
+//    });
+
+//    console.log(filteredLocations);
 
 }
 
-ko.applyBindings(new AppVMButton());
+ko.applyBindings(AppVMButton());
+ko.applyBindings(new AppVMLocations());
 
 // global variables
-var locations = [
-    {
-        title: 'Le Moulin du Diable restaurant',
-        location: {
-            lat: 48.579517,
-            lng: 7.744626
-        }
-    },
-    {
-        title: 'Notre Dame de Strasbourg Cathedral',
-        location: {
-            lat: 48.581125,
-            lng: 7.749384
-        }
-    },
-    {
-        title: 'Star Saint Exupéry Cinema',
-        location: {
-            lat: 48.583585,
-            lng: 7.743543
-        }
-    },
-    {
-        title: 'Botanical garden',
-        location: {
-            lat: 48.583577,
-            lng: 7.766685
-        }
-    },
-    {
-        title: 'Barrage Vauban Petite France',
-        location: {
-            lat: 48.580791,
-            lng: 7.741422
-        }
-    },
-    {
-        title: 'Chez Christian coffee shop',
-        location: {
-            lat: 48.583571,
-            lng: 7.745855
-        }
-    }
-]
+
 var map;
 var markers = [];
 var placeMarkers = [];
 
 
 function initMap() {
-    // Create a styles array to use with the map.
-    var styles = [
-    {
-        "featureType": "all",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "color": "#f5e4c1"
-            }
-        ]
-    },
-    {
-        "featureType": "all",
-        "elementType": "labels.text.fill",
-        "stylers": [
-            {
-                "gamma": 0.01
-            },
-            {
-                "lightness": 20
-            }
-        ]
-    },
-    {
-        "featureType": "all",
-        "elementType": "labels.text.stroke",
-        "stylers": [
-            {
-                "saturation": -31
-            },
-            {
-                "lightness": -33
-            },
-            {
-                "weight": 2
-            },
-            {
-                "gamma": 0.8
-            }
-        ]
-    },
-    {
-        "featureType": "all",
-        "elementType": "labels.icon",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "administrative",
-        "elementType": "all",
-        "stylers": [
-            {
-                "visibility": "on"
-            }
-        ]
-    },
-    {
-        "featureType": "administrative",
-        "elementType": "geometry.stroke",
-        "stylers": [
-            {
-                "visibility": "on"
-            }
-        ]
-    },
-    {
-        "featureType": "landscape",
-        "elementType": "all",
-        "stylers": [
-            {
-                "visibility": "on"
-            }
-        ]
-    },
-    {
-        "featureType": "landscape",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "lightness": 30
-            },
-            {
-                "saturation": 30
-            }
-        ]
-    },
-    {
-        "featureType": "landscape.man_made",
-        "elementType": "all",
-        "stylers": [
-            {
-                "visibility": "on"
-            },
-            {
-                "saturation": "0"
-            }
-        ]
-    },
-    {
-        "featureType": "landscape.man_made",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "visibility": "on"
-            }
-        ]
-    },
-    {
-        "featureType": "landscape.man_made",
-        "elementType": "geometry.fill",
-        "stylers": [
-            {
-                "visibility": "on"
-            },
-            {
-                "hue": "#ffa300"
-            },
-            {
-                "saturation": "-32"
-            },
-            {
-                "lightness": "20"
-            },
-            {
-                "gamma": "1.00"
-            }
-        ]
-    },
-    {
-        "featureType": "poi",
-        "elementType": "all",
-        "stylers": [
-            {
-                "visibility": "on"
-            },
-            {
-                "hue": "#ffaa00"
-            },
-            {
-                "saturation": "-26"
-            },
-            {
-                "lightness": "0"
-            },
-            {
-                "gamma": "1"
-            },
-            {
-                "weight": "0.39"
-            }
-        ]
-    },
-    {
-        "featureType": "poi",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "saturation": 20
-            }
-        ]
-    },
-    {
-        "featureType": "poi.park",
-        "elementType": "all",
-        "stylers": [
-            {
-                "visibility": "on"
-            },
-            {
-                "saturation": "51"
-            },
-            {
-                "lightness": "-6"
-            },
-            {
-                "weight": "0.01"
-            }
-        ]
-    },
-    {
-        "featureType": "poi.park",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "lightness": 20
-            },
-            {
-                "saturation": -20
-            }
-        ]
-    },
-    {
-        "featureType": "road",
-        "elementType": "all",
-        "stylers": [
-            {
-                "visibility": "on"
-            },
-            {
-                "hue": "#ffc000"
-            },
-            {
-                "saturation": "-100"
-            },
-            {
-                "lightness": "-46"
-            },
-            {
-                "gamma": "1.62"
-            },
-            {
-                "weight": "0.84"
-            }
-        ]
-    },
-    {
-        "featureType": "road",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "lightness": 10
-            },
-            {
-                "saturation": -30
-            }
-        ]
-    },
-    {
-        "featureType": "road",
-        "elementType": "geometry.stroke",
-        "stylers": [
-            {
-                "saturation": 25
-            },
-            {
-                "lightness": 25
-            }
-        ]
-    },
-    {
-        "featureType": "transit",
-        "elementType": "all",
-        "stylers": [
-            {
-                "visibility": "on"
-            },
-            {
-                "hue": "#fff400"
-            },
-            {
-                "saturation": "-100"
-            }
-        ]
-    },
-    {
-        "featureType": "water",
-        "elementType": "all",
-        "stylers": [
-            {
-                "lightness": -20
-            },
-            {
-                "visibility": "on"
-            }
-        ]
-    }
-];
     // Constructor creates a new map - only center and zoom are required.
     map = new google.maps.Map(document.getElementById('map'), {
         center: {
@@ -362,11 +65,8 @@ function initMap() {
             lng: 7.7523954
         },
         zoom: 16,
-        styles: styles
+        styles: style
     });
-
-    //add list of locations
-
 
 } // fin initMap
 
---
+++ js/mapLocations.js
@@ -0,0 +1,44 @@
+var locations = [
+    {
+        title: 'Le Moulin du Diable restaurant',
+        location: {
+            lat: 48.579517,
+            lng: 7.744626
+        }
+    },
+    {
+        title: 'Notre Dame de Strasbourg Cathedral',
+        location: {
+            lat: 48.581125,
+            lng: 7.749384
+        }
+    },
+    {
+        title: 'Star Saint Exupéry Cinema',
+        location: {
+            lat: 48.583585,
+            lng: 7.743543
+        }
+    },
+    {
+        title: 'Botanical garden',
+        location: {
+            lat: 48.583577,
+            lng: 7.766685
+        }
+    },
+    {
+        title: 'Barrage Vauban Petite France',
+        location: {
+            lat: 48.580791,
+            lng: 7.741422
+        }
+    },
+    {
+        title: 'Chez Christian coffee shop',
+        location: {
+            lat: 48.583571,
+            lng: 7.745855
+        }
+    }
+];
