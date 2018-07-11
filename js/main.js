9fba7a865145d6f52bea5a69638a721ae2cd4269
Nathalie <natg.inf@gmail.com>
2018-07-11 06:43:21 +0200

    Add list of places in sidebar

--- js/main.js
+++ js/main.js
@@ -1,81 +1,96 @@
  //(function () {
 
+ var Location = function (data) {
+     this.title = ko.observable(data.title);
+     this.lat = ko.observable(data.lat);
+     this.lng = ko.observable(data.lng);
+ };
 
-function AppVMButton () {
-    var btnOpen = 'Show places';
-    var btnClosed = 'Hide Places';
-    var sidebarClass = 'active';
-    btnText = ko.observable(btnOpen);
-    classActive = ko.observable("");
-    toggleButtonText = function() {
-        btnText() == btnClosed ? btnText(btnOpen) : btnText(btnClosed)
+ function AppVM() {
+     var self = this;
+
+     // ---- VM for button and sidebar ----
+     var btnOpen = 'Show places';
+     var btnClosed = 'Hide Places';
+     var sidebarClass = 'active';
+     btnText = ko.observable(btnOpen);
+     classActive = ko.observable("");
+     toggleButtonText = function () {
+         btnText() == btnClosed ? btnText(btnOpen) : btnText(btnClosed)
+     };
+     toggleButtonClass = ko.computed(function () {
+         return btnText() == btnClosed ? "open" : " ";
+     });
+     toggleMenuClass = ko.computed(function () {
+         return btnText() == btnClosed ? "active" : " ";
+     });
+
+
+     // ---- VM for locations generation and filter ----
+     self.locationsList = ko.observableArray([]);
+     self.filterText = ko.observable('');
+     self.currentSelection = ko.observable('');
+     locations.forEach(function (placeLocation) {
+         self.locationsList.push(new Location(placeLocation));
+     });
+     self.filteredLocations = ko.computed(function () {
+         if (!self.filterText()) {
+             return self.locationsList();
+         } else {
+             return self.locationsList().filter(location => location.title().toLowerCase().indexOf(self.filterText().toLowerCase()) > -1);
+         }
+     });
+     self.selectLocation = function(elem){
+//        console.log(self.currentSelection(elem.title));
+
+//         var theElem = document.getElementsByClassName;
+//         console.log();
+//         if (document.getElementsByClassName('selectedLocation')[0].innerHTML == elem.title() ) {
+//             console.log("le même");
+//         } else {
+//             console.log("pas le même");
+//         }
+
+         self.currentSelection(elem.title);
+//         console.log(this.parentNode.classList);
+//         if (self.currentSelection(elem.title)) {
+//            self.currentSelection() == "";
+//         } else {
+
+//         }
     };
-    toggleButtonClass = ko.computed(function(){
-        return btnText() == btnClosed ? "open" : " ";
-    });
-    toggleMenuClass = ko.computed(function() {
-        return btnText() == btnClosed ? "active" : " ";
-    });
-}
 
-//var Location = function(location){
-//    this.title = location.title;
-//    this.lat = location.lat;
-//    this.lng = location.lng;
-//}
+ }
 
-//https://www.strathweb.com/2012/07/knockout-js-pro-tips-working-with-observable-arrays/
-//https://stackoverflow.com/questions/47741328/filtering-list-with-knockout
-function AppVMLocations () {
-    var self = this;
-    self.filterText = ko.observable('');
-    self.allLocations = ko.observableArray(locations);
-    self.filteredLocations = ko.computed(function(){
-        if (self.filterText().length > 0) {
-            var locationsArray = self.allLocations();
-            return ko.utils.arrayFilter(teamsArray, function(allLocations){
-                return ko.utils.stringStartsWith(allLocations.locations.toLowerCase(), self.filterText());
-            });
-        }
-    }).extend({ throttle: 750 });
-
-//    locations.forEach(function(loc){
-//        self.allLocations.push(new Location(loc));
-//    });
-
-//    console.log(filteredLocations);
-
-}
-
-ko.applyBindings(AppVMButton());
-ko.applyBindings(new AppVMLocations());
-
-// global variables
-
-var map;
-var markers = [];
-var placeMarkers = [];
+ ko.applyBindings(new AppVM());
 
 
-function initMap() {
-    // Constructor creates a new map - only center and zoom are required.
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {
-            lat: 48.5816732,
-            lng: 7.7523954
-        },
-        zoom: 16,
-        styles: style
-    });
+ // global variables
 
-} // fin initMap
+ var map;
+ var markers = [];
+ var placeMarkers = [];
 
-function alertUser(errorType) {
-    switch (errorType) {
-    case 'mapsLoad':
-        console.log("There was a problem with google maps");
-        break;
-    }
-}
 
-//})();
+ function initMap() {
+     // Constructor creates a new map - only center and zoom are required.
+     map = new google.maps.Map(document.getElementById('map'), {
+         center: {
+             lat: 48.5816732,
+             lng: 7.7523954
+         },
+         zoom: 16,
+         styles: style
+     });
+
+ } // fin initMap
+
+ function alertUser(errorType) {
+     switch (errorType) {
+     case 'mapsLoad':
+         console.log("There was a problem with google maps");
+         break;
+     }
+ }
+
+ //})();
